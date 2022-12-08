21691c52cd8158e4fc22c3be6e4c70a483c19a58
mesut0606 <96373369+mesut0606@users.noreply.github.com>
2022-12-08 23:56:22 +0300

    patika dev

---
+++ readme.md
@@ -0,0 +1,6 @@
+Dairenin Alanını ve Çevresini Hesaplayan Program
+
+Java ile yarıçapını kullanıcıdan aldığınız dairenin alanını ve çevresini hesaplayan program
+ Alan Formülü = pi * r * r;
+ Çevre Formülü = 2 * pi * r;
+
---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+public static void main(String[] args) {
+        int r;
+        double pi = 3.14;
+
+    Scanner input = new Scanner(System.in);
+
+    System.out.print("Yarı çapını giriniz:");
+    r = input.nextInt();
+    double alan = pi * r * r;
+    double cevre = 2 * pi *r;
+    System.out.println("Dairenin alanı :" +alan);
+    System.out.println("Dairenin çevesi :" + cevre);
+
+
+
+
+
+
+        }
+        }
